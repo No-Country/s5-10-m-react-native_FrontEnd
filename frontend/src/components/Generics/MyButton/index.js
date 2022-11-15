15d39b031daf89e026fcb2435d11e65c215bd484
valentin-ruggieri <valeru.251@gmail.com>
2022-11-15 17:53:24 -0300

    Add generics components

---
+++ frontend/src/components/Generics/MyButton/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import PropTypes from "prop-types";
+
+const MyButton = ({ text, color, bgcolor, width, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: bgcolor ? bgcolor : "#fff",
+        borderWidth: bgcolor ? 0 : 1,
+        borderColor: !bgcolor && color,
+        width: width ? width : "100%",
+        borderRadius: 5,
+        padding: 5,
+        elevation: 5,
+      }}
+      onPress={onPress}
+    >
+      <Text style={{ color: color, textAlign: "center" }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+MyButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  bgcolor: PropTypes.string,
+  width: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+};
+
+export default MyButton;
--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
 import Styles from "./style";
+// import Button from "../../components/Generics/MyButton";
+// import Text from "../../components/Generics/MyText";
+// import fonts from "../../utils/fonts";
 
 const Home = () => {
   const styles = Styles;
+
   return (
     <View style={styles.container}>
       <Text>Pagina de Home</Text>
