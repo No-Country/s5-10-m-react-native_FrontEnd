153a47d6e4cb13e49b903cbcd8bb06c7b10ac9dc
orianadecaro <orianadecaro@gmail.com>
2022-11-16 19:54:58 -0300

    update

--- frontend/src/hooks/useDimensions.js
+++
@@ -1,7 +0,0 @@
-import { Dimensions } from "react-native";
-
-export const useDimensions = () => {
-  const heightScreen = Dimensions.get("screen").height;
-  const widthScreen = Dimensions.get("screen").width;
-  return { heightScreen, widthScreen };
-};
